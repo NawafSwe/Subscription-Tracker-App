1c632e72fe4d408e7a56b89353740e513eb659ed
NawafSwe <nawaf.jr9@hotmail.com>
2020-11-27 02:19:50 +0300

    Initial Commit

---
+++ Billing-Tracker/Billing_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Billing_TrackerApp.swift
+//  Billing-Tracker
+//
+//  Created by Nawaf B Al sharqi on 27/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct Billing_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
