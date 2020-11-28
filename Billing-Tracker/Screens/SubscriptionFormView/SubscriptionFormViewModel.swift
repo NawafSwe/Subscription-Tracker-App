56f7d53b49e3e420f8607cdb6f45a842a3ffe8c2
NawafSwe <nawaf.jr9@hotmail.com>
2020-11-28 04:43:45 +0300

    initing view model with the neeed it values

--- Billing-Tracker/Screens/SubscriptionFormView/SubscriptionFormViewModel.swift
+++ Billing-Tracker/Screens/SubscriptionFormView/SubscriptionFormViewModel.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+import SwiftUI
+import CoreData
+
+final class SubscriptionFormViewModel: ObservableObject {
+    @Environment (\.presentationMode) var presentationMode
+    @Published var providersList = Providers.providersList
+    @Published var selectedProvider = 0
+    @Published var subDescription = ""
+    @Published var subPrice:String = ""
+    @Published var date = Date()
+    @Published var selectedCycle = 0
+    @Published var cycleTypes = ["weekly","monthly","yearly"]
+    var calculatePrice:Double {
+        Double(subPrice) ?? 0.0
+    }
+    @Published var remindUser = false
+    
+}
+
