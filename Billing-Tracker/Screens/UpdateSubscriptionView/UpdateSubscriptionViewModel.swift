3bfa6695f5b6d8683f397c38a25b17dcf5ac6ff2
NawafSwe <nawaf.jr9@hotmail.com>
2021-01-05 17:59:47 +0300

    making init notification in update sub

--- Billing-Tracker/Screens/UpdateSubscriptionView/UpdateSubscriptionViewModel.swift
+++ Billing-Tracker/Screens/UpdateSubscriptionView/UpdateSubscriptionViewModel.swift
@@ -8,6 +8,7 @@
 import Foundation
 import SwiftUI
 import Combine
+import UserNotifications
 final class UpdateSubscriptionViewModel:ObservableObject{
     
     @Environment (\.presentationMode) var presentationMode
@@ -62,7 +63,7 @@ final class UpdateSubscriptionViewModel:ObservableObject{
         }
     }
     
-    // validation of form
+    //MARK:- isValidForm
     func isValidForm() ->Bool {
         if subscription.subscription.description.isEmpty{
             DispatchQueue.main.async {
@@ -79,6 +80,62 @@ final class UpdateSubscriptionViewModel:ObservableObject{
             return false
         }
         return true
+        
+    }
+    
+    func changeNotificationStatus(){
+        // if true init notification
+        // else delete notification from center
+        if self.subscription.subscription.notifyMe{
+            
+        }else {
+            
+        }
+    }
+    
+    
+    
+    
+    func initNotification(with date:Date){
+        let content = UNMutableNotificationContent()
+        content.title = "Subscription due date Reminder"
+        content.subtitle = subscription.subscription.name
+        
+        content.body = "\(subscription.subscription.notificationMessage)"
+        content.sound = UNNotificationSound.default
+        
+        //// Configure the recurring date.
+        var dateComponents = DateComponents()
+        /// to extract date component
+        let calendar = Calendar.current
+        let day = calendar.component(.day, from: date)
+        let year = calendar.component(.year, from: date)
+        let month = calendar.component(.month, from: date)
+        dateComponents.day = day
+        dateComponents.month = month
+        dateComponents.year = year
+        dateComponents.calendar = calendar
+        // end of config date
+        ////  Create the trigger as a repeating event.
+        
+        // choosing the trigger if the day is equal to one we want to notify user after an hour
+        // choose a random identifier
+        subscription.subscription.notificationId =  UUID().uuidString
+        var request:UNNotificationRequest
+        
+        if Date.daysDiffrent(start: Date(), end: date) == 1{
+            // not repeated
+            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 3600, repeats: false)
+            request = UNNotificationRequest(identifier:  subscription.subscription.notificationId , content: content, trigger: trigger)
+            
+        }else{
+            // not repeated
+            let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
+            request = UNNotificationRequest(identifier:  subscription.subscription.notificationId, content: content, trigger: trigger)
+            
+        }
+        // add our notification request
+        UNUserNotificationCenter.current().add(request)
         
     }
 }
